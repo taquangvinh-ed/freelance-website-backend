1a666a4c763045296a5c37c17b36d018ab1f8214
Ta Quang Vinh <tquangvinh.ed@gmail.com>
2025-09-13 23:12:25 +0700

    feat: add EducationsModel and ExperiencesModel

--- src/main/java/com/freelancemarketplace/backend/model/EducationsModel.java
+++ src/main/java/com/freelancemarketplace/backend/model/EducationsModel.java
@@ -8,7 +8,91 @@ public class EducationsModel extends BaseEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long education_id;
 
+    private String organization;
+    private String degree;
+    private String area_study;
+    private String start_date;
+    private String end_date;
+    private String description;
+
+
+
     @ManyToOne
     @JoinColumn(name = "freelancer_id")
     private FreelancersModel freelancerEducations;
+
+    public EducationsModel(Long education_id, String organization, String degree, String area_study, String start_date, String end_date, String description, FreelancersModel freelancerEducations) {
+        this.education_id = education_id;
+        this.organization = organization;
+        this.degree = degree;
+        this.area_study = area_study;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.description = description;
+        this.freelancerEducations = freelancerEducations;
+    }
+
+    public Long getEducation_id() {
+        return education_id;
+    }
+
+    public void setEducation_id(Long education_id) {
+        this.education_id = education_id;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getArea_study() {
+        return area_study;
+    }
+
+    public void setArea_study(String area_study) {
+        this.area_study = area_study;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+    public String getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(String end_date) {
+        this.end_date = end_date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public FreelancersModel getFreelancerEducations() {
+        return freelancerEducations;
+    }
+
+    public void setFreelancerEducations(FreelancersModel freelancerEducations) {
+        this.freelancerEducations = freelancerEducations;
+    }
 }
--- src/main/java/com/freelancemarketplace/backend/model/ExperiencesModel.java
+++ src/main/java/com/freelancemarketplace/backend/model/ExperiencesModel.java
@@ -8,6 +8,12 @@ public class ExperiencesModel extends BaseEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long experience_id;
 
+    private String title;
+    private String organization;
+    private String start_date;
+    private String end_date;
+    private String description;
+
 
     @ManyToOne
     @JoinColumn(name = "location_id")
@@ -17,4 +23,79 @@ public class ExperiencesModel extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "freelancer_id")
     private FreelancersModel freelancerExperiences;
+
+    public ExperiencesModel(Long experience_id, String title, String organization, String start_date, String end_date, String description, LocationsModel experience_location, FreelancersModel freelancerExperiences) {
+        this.experience_id = experience_id;
+        this.title = title;
+        this.organization = organization;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.description = description;
+        this.experience_location = experience_location;
+        this.freelancerExperiences = freelancerExperiences;
+    }
+
+    public Long getExperience_id() {
+        return experience_id;
+    }
+
+    public void setExperience_id(Long experience_id) {
+        this.experience_id = experience_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+    public String getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(String end_date) {
+        this.end_date = end_date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocationsModel getExperience_location() {
+        return experience_location;
+    }
+
+    public void setExperience_location(LocationsModel experience_location) {
+        this.experience_location = experience_location;
+    }
+
+    public FreelancersModel getFreelancerExperiences() {
+        return freelancerExperiences;
+    }
+
+    public void setFreelancerExperiences(FreelancersModel freelancerExperiences) {
+        this.freelancerExperiences = freelancerExperiences;
+    }
 }
