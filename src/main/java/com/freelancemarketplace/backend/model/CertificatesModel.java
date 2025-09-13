1201de25a9f8bfb5aa84793b0bce4983820955f2
Ta Quang Vinh <tquangvinh.ed@gmail.com>
2025-09-13 23:04:45 +0700

    feat: add neccessary method for CertificatesModel

--- src/main/java/com/freelancemarketplace/backend/model/CertificatesModel.java
+++ src/main/java/com/freelancemarketplace/backend/model/CertificatesModel.java
@@ -16,4 +16,61 @@ public class CertificatesModel extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "freelancer_id")
     private FreelancersModel freelancerCertificates;
+
+    public CertificatesModel(Long certificate_id, String title, String organization, String date_obtained, String certificate_url, FreelancersModel freelancerCertificates) {
+        this.certificate_id = certificate_id;
+        this.title = title;
+        this.organization = organization;
+        this.date_obtained = date_obtained;
+        this.certificate_url = certificate_url;
+        this.freelancerCertificates = freelancerCertificates;
+    }
+
+    public Long getCertificate_id() {
+        return certificate_id;
+    }
+
+    public void setCertificate_id(Long certificate_id) {
+        this.certificate_id = certificate_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getDate_obtained() {
+        return date_obtained;
+    }
+
+    public void setDate_obtained(String date_obtained) {
+        this.date_obtained = date_obtained;
+    }
+
+    public String getCertificate_url() {
+        return certificate_url;
+    }
+
+    public void setCertificate_url(String certificate_url) {
+        this.certificate_url = certificate_url;
+    }
+
+    public FreelancersModel getFreelancerCertificates() {
+        return freelancerCertificates;
+    }
+
+    public void setFreelancerCertificates(FreelancersModel freelancerCertificates) {
+        this.freelancerCertificates = freelancerCertificates;
+    }
 }
