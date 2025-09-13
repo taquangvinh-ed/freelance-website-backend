a5e5783eff9b7588ce67ff4f2919bbc2e7d0f080
Ta Quang Vinh <tquangvinh.ed@gmail.com>
2025-09-13 09:15:12 +0700

    feat: add QandAModel

---
+++ src/main/java/com/freelancemarketplace/backend/model/AdminsModel.java
@@ -0,0 +1,17 @@
+package com.freelancemarketplace.backend.model;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class AdminsModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long admin_id;
+
+    @OneToMany(mappedBy = "admin")
+    private Set<QandAModel> questions_and_answers;
+
+
+}
---
+++ src/main/java/com/freelancemarketplace/backend/model/QandAModel.java
@@ -0,0 +1,66 @@
+package com.freelancemarketplace.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class QandAModel extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long qanda_id;
+
+    private String question;
+    private String answer;
+    private String tag;
+
+    @ManyToOne
+    @JoinColumn(name = "admin_id")
+    private AdminsModel admin;
+
+    public QandAModel(Long qanda_id, String question, String answer, String tag, AdminsModel admin) {
+        this.qanda_id = qanda_id;
+        this.question = question;
+        this.answer = answer;
+        this.tag = tag;
+        this.admin = admin;
+    }
+
+    public Long getQanda_id() {
+        return qanda_id;
+    }
+
+    public void setQanda_id(Long qanda_id) {
+        this.qanda_id = qanda_id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public AdminsModel getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(AdminsModel admin) {
+        this.admin = admin;
+    }
+}
