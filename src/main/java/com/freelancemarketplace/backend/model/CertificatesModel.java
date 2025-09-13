0d6f0910df8abe0a529fbbc3fcd2de52ad3a2593
Ta Quang Vinh <tquangvinh.ed@gmail.com>
2025-09-13 23:03:38 +0700

    feat: add CertificatesModel

--- src/main/java/com/freelancemarketplace/backend/model/CertificatesModel.java
+++ src/main/java/com/freelancemarketplace/backend/model/CertificatesModel.java
@@ -3,10 +3,15 @@ package com.freelancemarketplace.backend.model;
 import jakarta.persistence.*;
 
 @Entity
-public class CertificatesModel {
+public class CertificatesModel extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long certificate_id;
+
+    private String title;
+    private String organization;
+    private String date_obtained;
+    private String certificate_url;
 
     @ManyToOne
     @JoinColumn(name = "freelancer_id")
