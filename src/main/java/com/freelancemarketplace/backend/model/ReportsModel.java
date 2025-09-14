5d961f92017909040d6553d7f5851524f52577e5
Ta Quang Vinh <tquangvinh.ed@gmail.com>
2025-09-14 19:54:26 +0700

    feat: add ReportsModel

--- src/main/java/com/freelancemarketplace/backend/model/ReportsModel.java
+++ src/main/java/com/freelancemarketplace/backend/model/ReportsModel.java
@@ -8,6 +8,10 @@ public class ReportsModel extends BaseEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long report_id;
 
+
+    private String report_category;
+    private String report_reason;
+
     //The freelancer who is being reported
     @ManyToOne
     @JoinColumn(name = "freelancer_id")
@@ -22,4 +26,60 @@ public class ReportsModel extends BaseEntity{
     @JoinColumn(name = "client_id")
     private ClientsModel client_reports;
 
+    public ReportsModel(Long report_id, String report_category, String report_reason, FreelancersModel freelancerReports, CompaniesModel companyReports, ClientsModel client_reports) {
+        this.report_id = report_id;
+        this.report_category = report_category;
+        this.report_reason = report_reason;
+        this.freelancerReports = freelancerReports;
+        this.companyReports = companyReports;
+        this.client_reports = client_reports;
+    }
+
+    public Long getReport_id() {
+        return report_id;
+    }
+
+    public void setReport_id(Long report_id) {
+        this.report_id = report_id;
+    }
+
+    public String getReport_category() {
+        return report_category;
+    }
+
+    public void setReport_category(String report_category) {
+        this.report_category = report_category;
+    }
+
+    public String getReport_reason() {
+        return report_reason;
+    }
+
+    public void setReport_reason(String report_reason) {
+        this.report_reason = report_reason;
+    }
+
+    public FreelancersModel getFreelancerReports() {
+        return freelancerReports;
+    }
+
+    public void setFreelancerReports(FreelancersModel freelancerReports) {
+        this.freelancerReports = freelancerReports;
+    }
+
+    public CompaniesModel getCompanyReports() {
+        return companyReports;
+    }
+
+    public void setCompanyReports(CompaniesModel companyReports) {
+        this.companyReports = companyReports;
+    }
+
+    public ClientsModel getClient_reports() {
+        return client_reports;
+    }
+
+    public void setClient_reports(ClientsModel client_reports) {
+        this.client_reports = client_reports;
+    }
 }
